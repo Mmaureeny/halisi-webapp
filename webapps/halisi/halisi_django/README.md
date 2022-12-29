c76e2d7bb76500b688770c52359935a483eaa881
Mmaureeny <muthonimaureen4@outlook.com>
2022-12-29 08:59:45 -0800

    deploying

---
+++ webapps/halisi/halisi_django/README.md
@@ -0,0 +1,3 @@
+The goal with this research project is to learn the essential parts of the backend of an ecommerce application. My application will implement the features using Django and Vue.
+Author
+Mmaureeny-github.com
---
+++ webapps/halisi/halisi_django/order/views.py
@@ -0,0 +1,60 @@
+import stripe
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.shortcuts import render
+
+from rest_framework import status, authentication, permissions
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from .models import Order, OrderItem
+from .serializers import OrderSerializer, MyOrderSerializer
+
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+@api_view(['POST'])
+@authentication_classes([authentication.TokenAuthentication])
+@permission_classes([permissions.IsAuthenticated])
+def checkout(request):
+    serializer = OrderSerializer(data=request.data)
+
+    if serializer.is_valid():
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        paid_amount = sum(item.get('quantity') * item.get('product').price for item in serializer.validated_data['items'])
+
+        try:
+            charge = stripe.Charge.create(
+                amount=int(paid_amount * 100),
+                currency='USD',
+                description='Charge from Halisi',
+                source=serializer.validated_data['stripe_token']
+            )
+
+            serializer.save(user=request.user, paid_amount=paid_amount)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except Exception:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def email(request):
+    subject = 'Thank you for your purchase'
+    message = ' it  means a world to us '
+    email_from = settings.EMAIL_HOST_USER
+    recipient_list = ['receiver@gmail.com',]
+    send_mail( subject, message, email_from, recipient_list )
+    return redirect('redirect to a new page')
+
+class OrdersList(APIView):
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, format=None):
+        orders = Order.objects.filter(user=request.user)
+        serializer = MyOrderSerializer(orders, many=True)
+        return Response(serializer.data)
